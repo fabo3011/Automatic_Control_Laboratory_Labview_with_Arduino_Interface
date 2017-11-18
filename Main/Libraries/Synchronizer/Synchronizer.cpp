0e8e240af536555458f71eafae39dae0de9d02e9
unknown <a01209914@itesm.mx>
2017-11-18 07:26:58 -0600

    Create Synchronizer Library

---
+++ Main/Libraries/Synchronizer/Synchronizer.cpp
@@ -0,0 +1,11 @@
+/*
+  Synchronizer.h - Library to synchronize the period of the Main program to match the Controller's sampling rate
+  Created by fgg, November 18, 2017.
+  Released into the public domain.
+*/
+
+#include "Arduino.h"
+#include "Synchronizer.h"
+
+Synchronizer::Synchronizer(){}
+
---
+++ Main/Libraries/Synchronizer/Synchronizer.h
@@ -0,0 +1,18 @@
+/*
+  Synchronizer.h - Library to synchronize the period of the Main program to match the Controller's sampling rate
+  Created by fgg, November 18, 2017.
+  Released into the public domain.
+*/
+#ifndef Synchronizer_h
+#define Synchronizer_h
+
+#include "Arduino.h"
+
+class Synchronizer{
+  public:
+    Synchronizer();
+  
+  private:
+   
+};
+#endif
